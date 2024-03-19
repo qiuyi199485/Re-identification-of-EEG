194b57ba21e782ccee3d4d2a9eab8ee752f10fb9
qiuyi199485 <qiuyi199485@gmail.com>
2024-03-19 18:22:24 +0800

    Update edf_info_mne.py

--- edf_info_mne.py
+++ edf_info_mne.py
@@ -1,7 +1,7 @@
 import mne
 
 # load EDF
-raw = mne.io.read_raw_edf('C://Users/49152/Desktop/MA/Code/000/00000001_aaaaaaab/s001_2002/02_tcp_le/00000001_aaaaaaab_s001_t000.edf', preload=True)
+raw = mne.io.read_raw_edf('C://Users/49152/Desktop/MA/Code/000/aaaaaaab/s001_2002/02_tcp_le/aaaaaaab_s001_t000.edf', preload=True)
 
 
 print(raw.info)
