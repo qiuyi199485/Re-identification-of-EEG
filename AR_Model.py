b3ba2a1fe7e75f49ae7d7e5c407109dc8ec10d02
qiuyi199485 <qiuyi199485@gmail.com>
2024-06-26 23:15:51 +0800

    update

--- AR_Model.py
+++ AR_Model.py
@@ -11,8 +11,8 @@ import pandas as pd
 
 sys.path.insert(1, 'C:\\Users\\49152\\Desktop\\MA\\Code')                    # 允许脚本导入一个特定路径下的自定义Python脚本，例如settings模块和tools模块里的函数。
 import settings
-from tools import test_edf_corrupted_info, get_date_edf                      ## (edf_corrupted, edf_info) false没坏，edf_metadata; edf measurment date yyyy-mm-dd
-
+from tools import test_edf_corrupted_info, get_date_edf                  ## (edf_corrupted, edf_info) false没坏，edf_metadata; edf measurment date yyyy-mm-dd
+from getDataset import get_patients   
 from statsmodels.tsa.ar_model import AutoReg
 
 
@@ -24,7 +24,7 @@ def read_eeg_from_edf(file_path):
 
 
 # Fit AR model 
-def fit_ar_model(eeg_data, lags=10):
+def fit_ar_model(eeg_data, lags=6):
     ar_features = []
     for channel_data in eeg_data:
         model = AutoReg(channel_data, lags=lags)
@@ -34,23 +34,27 @@ def fit_ar_model(eeg_data, lags=10):
     return ar_features
 
 # Extract AR features
-def process_and_extract_features(patients):
+def process_and_extract_features(patients, max_patients=100):
     features_dict = {}
+    patient_count = 0
     for patient_id, records in patients.items():
+        if patient_count >= max_patients:
+            break
         features_dict[patient_id] = []
         for record in records:
             date_of_the_take, session_id_take, path_to_edf, edf_info = record
             eeg_data = read_eeg_from_edf(path_to_edf)
             ar_features = fit_ar_model(eeg_data)
             features_dict[patient_id].append((session_id_take, ar_features))
+        patient_count += 1
     return features_dict
 
-# Example usage
-path = 'your_dataset_path'
+# Example 
+path = 'C:\\Users\\49152\\Desktop\\MA\\Code'
 patients = get_patients(path)
-features_dict = process_and_extract_features(patients)
+features_dict = process_and_extract_features(patients, max_patients=100)
 
-# Convert the extracted features to a DataFrame
+# Features to  DataFrame
 def convert_features_to_dataframe(features_dict):
     feature_list = []
     for patient_id, records in features_dict.items():
@@ -62,5 +66,5 @@ def convert_features_to_dataframe(features_dict):
 
 features_df = convert_features_to_dataframe(features_dict)
 
-# Display the DataFrame with AR features
+# Display AR features
 print(features_df)
